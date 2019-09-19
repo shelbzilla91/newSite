2e7f07a54d0206703c483fc2316a80dbcb8d442e
Shelby Baker <shelbymbaker91@gmail.com>
2019-09-19 16:14:34 -0500

    started on slider

--- gatsby_mysite/src/components/aboutContainer.js
+++ gatsby_mysite/src/components/aboutContainer.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react'
 import '../components/aboutContainer.scss'
 import { Message } from 'semantic-ui-react'
 import FactsContainer from './factsContainer'
+import Slider from './slider'
 
 export default class aboutContainer extends Component {
   
   render() {
     return (
-      <div>
+      <div >
       <div className='aboutContainer'>
         <div className = 'profile'>
 
         </div>
    
-        <div>  <Message className= 'name' size='big'>This is big</Message></div>
-        <div>njkbskjbvks</div>
+        <div className =" facts">  <Message className= 'name' size='large'>This is big</Message></div>
+        <div className= 'titles'>
+          
+          <Slider/>
+        </div>
        
       </div>
       <FactsContainer/>
